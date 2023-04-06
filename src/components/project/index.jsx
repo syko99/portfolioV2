52048b60b04581baa69587c20b32fbce38a4e7e3
syko99 <sykong.yang@hotmail.com>
2023-04-06 16:20:04 -0500

    add content to project cards

--- src/components/project/index.jsx
+++ src/components/project/index.jsx
@@ -1,14 +1,51 @@
-import Project from "./project"
+import Project from './project'
+import welpImg from '../../assets/project_images/welp.jpg'
+import pixelSketcherImg from '../../assets/project_images/pixelSketcher.jpg'
+import taskrImg from '../../assets/project_images/taskr.png'
+import castforecastImg from '../../assets/project_images/castforecast.png'
 
 const Projects = () => {
+    let projectList = [
+        {
+            name: 'Pixel Sketcher',
+            image: pixelSketcherImg,
+            description: 'A sketching web application made with HTML, CSS, and Javascript.',
+            live: '',
+            code: '',
+        },
+        {
+            name: 'Welp',
+            image: welpImg,
+            description: 'A full-stack review website using PHP, SQL, Javascript, HTML, and CSS.',
+            live: '',
+            code: '',
+        },
+        {
+            name: 'Taskr',
+            image: taskrImg,
+            description:
+                'The ultimate to-do list app designed to help you organize your life! Say goodbye to traditional paper lists and sticky notes cluttering your desk, our app is all you need to stay organized and productive.',
+            live: 'https://taskrapp.netlify.app',
+            code: '',
+        },
+        {
+            name: 'Castforecast',
+            image: castforecastImg,
+            description:
+                "You don't need to be a meteorologist to understand the data. With just a few clicks, you can see the forecast for the next few days and get a sense of the overall weather patterns in your area.",
+            live: 'https://castforecast.netlify.app',
+            code: '',
+        },
+    ]
+
+    let projectComponents = projectList.reverse().map((project) => {
+        return <Project project={project} />
+    })
 
     return (
-        <div className="w-full grid gap-4">
+        <div className='w-full grid gap-4'>
             <hr />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
+            {projectComponents}
         </div>
     )
 }
--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -1,12 +1,20 @@
-const Project = () => {
+const Project = ({ project }) => {
     return (
-        <div className='w-full bg-white rounded-lg p-4 flex flex-col gap-4 shadow-md'>
-            <h3>Project Name</h3>
-            <div className='grid place-items-center'>Image</div>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, vero!</p>
-            <div className='flex gap-4 justify-center items-center'>
-                <a href="" className="w-full"><button className="w-full p-1 border rounded-lg">Live</button></a>
-                <a href="" className="w-full"><button className="w-full p-1 border rounded-lg">Code</button></a>
+        <div className='w-full bg-white rounded-lg flex flex-col shadow-md overflow-hidden'>
+            <div className='grid place-items-center max-w-lg mx-auto'>
+                <img src={project.image} alt={`${project.name}` + ' image'} />
+            </div>
+            <div className="flex flex-col gap-4 p-4">
+                <h3 className='text-2xl font-bold capitalize'>{project.name}</h3>
+                <p>{project.description}</p>
+                <div className='flex gap-4 justify-center items-center'>
+                    <a href={project.live} target='_' className='w-full'>
+                        <button className='w-full p-1 border rounded-lg'>Live</button>
+                    </a>
+                    <a href={project.code} target='_' className='w-full'>
+                        <button className='w-full p-1 border rounded-lg'>Code</button>
+                    </a>
+                </div>
             </div>
         </div>
     )

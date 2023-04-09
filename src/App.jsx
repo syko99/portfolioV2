68a80d106a2747a396cbd1bce238ad4cc5a424aa
syko99 <sykong.yang@hotmail.com>
2023-04-09 11:36:56 -0500

    add session storage to save light mode preference

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,24 @@ import Projects from './components/project'
 import Skills from './components/skills'
 
 function App() {
-    const [lightMode, setLightMode] = useState(true)
+    const [lightMode, setLightMode] = useState(getLightModeSession() === 'true')
 
     function toggleLightMode() {
-        lightMode === true ? setLightMode(false) : setLightMode(true)
+        lightMode == true ? setLightMode(false) : setLightMode(true)
+        updateLightModeSession()
+    }
+
+    function getLightModeSession() {
+        if (sessionStorage.getItem('mode') === null) {
+            sessionStorage.setItem('mode', 'true')
+        }
+        return sessionStorage.getItem('mode')
+    }
+
+    function updateLightModeSession() {
+        sessionStorage.getItem('mode') === 'true'
+            ? sessionStorage.setItem('mode', 'false')
+            : sessionStorage.setItem('mode', 'true')
     }
 
     return (
@@ -21,10 +35,10 @@ function App() {
             <div className='flex flex-col bg-zinc-100 mx-auto min-h-screen dark:bg-zinc-950'>
                 <Header toggleLightMode={toggleLightMode} />
                 <main className='flex-grow p-4 max-w-screen-2xl mx-auto relative grid gap-4 grid-areas-mobile sm:grid-areas-tablet lg:grid-areas-desktop lg:grid-rows-desktop lg:grid-cols-desktop 2xl:grid-cols-desktop-wide'>
-                  <About />
-                  <Work />
-                  <Skills />
-                  <Projects />
+                    <About />
+                    <Work />
+                    <Skills />
+                    <Projects />
                 </main>
                 <Footer />
             </div>
